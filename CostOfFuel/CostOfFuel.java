1bd14a4e863825bbb6b4b3fa200f3e256e667297
Achitu24 <adrian.chitu7@gmail.com>
2019-01-22 16:38:50 +0200

    readme

--- CostOfFuel/CostOfFuel.java
+++ CostOfFuel/CostOfFuel.java
@@ -1,9 +1,9 @@
 import java.util.*;
 
 public class CostOfFuel {
-	public static void main (String[] args){
+	public static void main(String[] args) {
 		Scanner Input = new Scanner(System.in);
-		
+
 		System.out.println("Please type in your Monday cost :");
 		double mondayFuelCost = Input.nextDouble();
 		System.out.println("Please type in your Tuesday cost :");
@@ -14,19 +14,14 @@ public class CostOfFuel {
 		double thursdayFuelCost = Input.nextDouble();
 		System.out.println("Please type in your Friday cost: ");
 		double fridayFuelCost = Input.nextDouble();
-		
-		
-		Calculations Cost = new Calculations(mondayFuelCost, tuesdayFuelCost, wednesdayFuelCost, 
-																						 thursdayFuelCost, fridayFuelCost);
-			double total = Cost.getTotalCost();
-			double average = Cost.getAverageCost();
-			
-			System.out.println("Your total cost is: "+ total + " Pounds");
-			System.out.println("Your average cost is: " + average + " Pounds");
-			
-		
-		
-		
+
+		Calculations Cost = new Calculations(mondayFuelCost, tuesdayFuelCost, wednesdayFuelCost, thursdayFuelCost,fridayFuelCost);
+		double total = Cost.getTotalCost();
+		double average = Cost.getAverageCost();
+
+		System.out.println("Your total cost is: " + total + " Pounds");
+		System.out.println("Your average cost is: " + average + " Pounds");
+
 	}
-	
+
 }
--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Java Beginner Files
+## Worked examples for the Comp101 Module
 
 - Carpet Cost - A Java Program that calculates and prints the floor area of an apartment
+- Cost of Fuel - A Java Program that calculates the total and the average cost of fuel for 1 week
 - Giant - A Java program to print my Computer Science login username on the screen
 - KD - A Java program to find nearest neighbor using KD Tree implementation
 - Late Penalties - A Java program that calculates and displays information about penalties for late submission for a piece of coursework
 - Robot Movement - A Java program to calculate and print out various properties of a Robot
-- Swimming Pool - A Java Program that calcualates the volume of a swimming Pool
+- Swimming Pool - A Java program that calcualates the volume of a swimming pool and its fill time
+- Tunnel Toll - A Java program that determines and prints the vehicle class, the vehicle type and vehicle charge
